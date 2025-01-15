d7c56efd14fcbb3fda2d64babeec1f763a00330c
Robert <96136484+Biggyhands@users.noreply.github.com>
2025-01-14 23:13:26 -0500

    ResultsCardComponent Updated
    Country Name object added
    Component styles added

--- app/src/components/ResultsCardComponent.jsx
+++ app/src/components/ResultsCardComponent.jsx
@@ -2,25 +2,26 @@ import React from "react";
 
 export const ResultsCardComponent = ({ data }) => {
   return (
-    <div>
-      <nav>
+    <section className="results-container">
+      <nav className="results-navbar">
         <h1>Name Investigator App</h1>
+        <button>Back To Search Results</button>
       </nav>
-      <section>
+      <section className="results-card-container">
         {data.personalNames.map((result, index) => (
-          <div key={index}>
-            <section>
+          <div key={index} className="results-card">
+            <section className="results-info">
               <h4>Country Origin: {result.countryOrigin}</h4>
               <p>Probability: {result.probabilityCalibrated}</p>
             </section>
-            <section>
+            <section className="results-info">
               <h4>Country Origin Alternative: {result.countryOriginAlt}</h4>
               <p>Probability: {result.probabilityAltCalibrated}</p>
             </section>
           </div>
         ))}
       </section>
-    </div>
+    </section>
   );
 };
 
